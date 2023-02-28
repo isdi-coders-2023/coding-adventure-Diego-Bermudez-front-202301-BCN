bf514706218cd9786b20dad82ff3cd0efc43e494
DI3BOSS <diegofbermudez@gmail.com>
2023-02-28 15:30:00 +0100

    Wrap the rendered Button component with ThemeProvider in its test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,12 +1,18 @@
 import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
 import Button from "./Button";
+import theme from "../../styles/theme";
 
 describe("Given a Button component", () => {
   describe("When it is call with the text 'Sign up'", () => {
     test("Then it should show a button with the text 'Sign up'", () => {
       const buttonText = "Sign up";
 
-      render(<Button text={"Sign up"} />);
+      render(
+        <ThemeProvider theme={theme}>
+          <Button text={buttonText} />
+        </ThemeProvider>
+      );
 
       const expectedButton = screen.getByRole("button", {
         name: buttonText,
